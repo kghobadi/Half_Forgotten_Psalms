1443f9b58d55ba6ee737cc2be40597084aff6c81
kghobadi <kasghobadi@gmail.com>
2021-01-29 22:48:03 -0500

    sounds and basic movement setup

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController charController;
+
+    private Vector3 movement;
+    public float walkSpeed;
+    
+    void Start()
+    {
+        charController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        MoveInputs();
+    }
+
+    void MoveInputs()
+    {
+        float moveForwardBackward = Input.GetAxis("Vertical") * walkSpeed;
+        float moveLeftRight = Input.GetAxis("Horizontal") * walkSpeed;
+
+        movement = new Vector3(moveLeftRight, 0, moveForwardBackward);
+
+        charController.Move(movement * Time.deltaTime);
+    }
+}
